5305af450240568cb208670a3ed80570216d6683
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2024-07-30 00:21:45 -0600

    Remove friendly group information from SPIKED response

--- pkg/controller/spiked.go
+++ pkg/controller/spiked.go
@@ -1,10 +1,8 @@
 package controller
 
 import (
-	"github.com/dharmab/skyeye/internal/conf"
 	"github.com/dharmab/skyeye/pkg/brevity"
 	"github.com/martinlindhe/unit"
-	"github.com/paulmach/orb/geo"
 	"github.com/rs/zerolog/log"
 )
 
@@ -12,6 +10,7 @@ import (
 func (c *controller) HandleSpiked(r *brevity.SpikedRequest) {
 	logger := log.With().Str("callsign", r.Callsign).Type("type", r).Float64("bearing", r.Bearing.Degrees()).Logger()
 	logger.Debug().Msg("handling request")
+
 	requestorTrackfile := c.scope.FindCallsign(r.Callsign)
 	if requestorTrackfile == nil {
 		logger.Info().Msg("no trackfile found for requestor")
@@ -20,59 +19,25 @@ func (c *controller) HandleSpiked(r *brevity.SpikedRequest) {
 	}
 	requestorLocation := requestorTrackfile.LastKnown().Point
 	arc := unit.Angle(30) * unit.Degree
-	hostileGroup := c.scope.FindNearestGroupInCone(requestorLocation, r.Bearing, arc, c.hostileCoalition(), brevity.FixedWing)
-	friendlyGroup := c.scope.FindNearestGroupInCone(requestorLocation, r.Bearing, arc, c.coalition, brevity.FixedWing)
-	resp := brevity.SpikedResponse{
-		Callsign: r.Callsign,
-		Bearing:  r.Bearing,
+	nearestGroup := c.scope.FindNearestGroupInCone(requestorLocation, r.Bearing, arc, c.hostileCoalition(), brevity.FixedWing)
+
+	if nearestGroup == nil {
+		logger.Info().Msg("no hostile groups found within spike cone")
+		c.out <- brevity.SpikedResponse{Callsign: r.Callsign, Status: false}
+		return
 	}
 
-	if hostileGroup == nil && friendlyGroup == nil {
-		logger.Debug().Msg("no groups found within spike cone")
-		resp.Status = false
-		resp.Aspect = brevity.UnknownAspect
-		resp.Declaration = brevity.Clean
-	} else {
-		resp.Status = true
-		var nearestGroup brevity.Group
-		if hostileGroup != nil && friendlyGroup != nil {
-			logger.Debug().Msg("both hostile and friendly groups found within spike cone")
-			if hostileGroup.BRAA().Range() < friendlyGroup.BRAA().Range() {
-				logger.Debug().Msg("hostile group is closer")
-				nearestGroup = hostileGroup
-				resp.Declaration = brevity.Hostile
-			} else {
-				logger.Debug().Msg("friendly group is closer")
-				nearestGroup = friendlyGroup
-				resp.Declaration = brevity.Friendly
-			}
-			// check if hostile and friendly within 3nm
-			hostilePoint := geo.PointAtBearingAndDistance(requestorLocation, hostileGroup.BRAA().Bearing().Degrees(), hostileGroup.BRAA().Range().Meters())
-			friendlyPoint := geo.PointAtBearingAndDistance(requestorLocation, friendlyGroup.BRAA().Bearing().Degrees(), friendlyGroup.BRAA().Range().Meters())
-			if geo.Distance(hostilePoint, friendlyPoint) < (conf.DefaultMarginRadius).Meters() {
-				logger.Debug().Msg("hostile and friendly groups are within 3nm (furball)")
-				resp.Declaration = brevity.Furball
-				resp.Aspect = brevity.UnknownAspect
-			}
-		} else if hostileGroup != nil {
-			logger.Debug().Msg("hostile group found within spike cone")
-			nearestGroup = hostileGroup
-			resp.Declaration = brevity.Hostile
-			resp.Status = true
-		} else {
-			logger.Debug().Msg("friendly group found within spike cone")
-			nearestGroup = friendlyGroup
-			resp.Declaration = brevity.Friendly
-		}
-
-		_range := nearestGroup.BRAA().Range()
-		resp.Range = &_range
-		altitude := nearestGroup.BRAA().Altitude()
-		resp.Altitude = &altitude
-		resp.Aspect = nearestGroup.BRAA().Aspect()
-		resp.Track = nearestGroup.Track()
-		resp.Contacts = nearestGroup.Contacts()
+	logger = logger.With().Any("group", nearestGroup).Logger()
+	logger.Debug().Msg("hostile group found within spike cone")
+	c.out <- brevity.SpikedResponse{
+		Callsign:    r.Callsign,
+		Status:      true,
+		Bearing:     r.Bearing,
+		Range:       nearestGroup.BRAA().Range(),
+		Altitude:    nearestGroup.BRAA().Altitude(),
+		Aspect:      nearestGroup.BRAA().Aspect(),
+		Track:       nearestGroup.Track(),
+		Declaration: brevity.Hostile,
+		Contacts:    nearestGroup.Contacts(),
 	}
-
-	c.out <- resp
 }
