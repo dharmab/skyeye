da67a6f0bb21e491ba8c05d1701da884416656ef
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2024-08-04 18:04:16 -0600

    Limit recognition to first 256k elements of sample

--- pkg/recognizer/whisper.go
+++ pkg/recognizer/whisper.go
@@ -20,8 +20,15 @@ func NewWhisperRecognizer(model *whisper.Model) Recognizer {
 	return &whisperRecognizer{model: *model}
 }
 
+const maxSize = 256 * 1024
+
 // Recognize implements [Recognizer.Recognize] using whisper.cpp
 func (r *whisperRecognizer) Recognize(sample []float32) (string, error) {
+	if len(sample) > maxSize {
+		log.Warn().Int("byteLength", len(sample)).Int("maxSize", maxSize).Msg("clamping sample to maximum size")
+		sample = sample[:maxSize]
+	}
+
 	wCtx, err := r.model.NewContext()
 	wCtx.SetInitialPrompt("You are a Ground Control Intercept (GCI) operator. You receive text in the format ['ANYFACE' / GCI CALLSIGN] [PILOT CALLSIGN] [DIGITS] ['RADIO', 'ALPHA', 'BOGEY', 'PICTURE', 'DECLARE', 'SNAPLOCK', or 'SPIKED'] [ARGUMENTS]. Parse numbers as digits.")
 	if err != nil {
