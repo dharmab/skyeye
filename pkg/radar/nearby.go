a394c102285efa99ff94be9dc17db3896fc67bd9
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2024-08-12 19:19:48 -0600

    remove duplicate groups returned by findNearbyGroups

--- pkg/radar/nearby.go
+++ pkg/radar/nearby.go
@@ -14,14 +14,21 @@ import (
 func (s *scope) findNearbyGroups(interest orb.Point, minAltitude, maxAltitude, radius unit.Length, coalition coalitions.Coalition, filter brevity.ContactCategory) []*group {
 	circle := geo.NewBoundAroundPoint(interest, float64(radius.Meters()))
 	groups := make([]*group, 0)
+	visited := make(map[uint32]struct{})
 	itr := s.contacts.itr()
 	for itr.next() {
 		trackfile := itr.value()
+		if _, ok := visited[trackfile.Contact.UnitID]; ok {
+			continue
+		}
 		isMatch := s.isMatch(trackfile, coalition, filter)
 		inCircle := circle.Contains(trackfile.LastKnown().Point)
 		inStack := minAltitude <= trackfile.LastKnown().Altitude && trackfile.LastKnown().Altitude <= maxAltitude
 		if isMatch && inCircle && inStack {
 			grp := s.findGroupForAircraft(trackfile)
+			for id := range grp.UnitIDs() {
+				visited[uint32(id)] = struct{}{}
+			}
 			groups = append(groups, grp)
 		}
 	}
