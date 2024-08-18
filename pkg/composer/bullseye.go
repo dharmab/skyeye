b99367e276a947f84058f0e0f13de4cb1e5e976d
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2024-08-18 00:35:40 -0600

    Say 'at bullseye' if coordinate is within 5NM of bullseye

--- pkg/composer/bullseye.go
+++ pkg/composer/bullseye.go
@@ -13,6 +13,12 @@ func (c *composer) ComposeBullseye(bullseye brevity.Bullseye) NaturalLanguageRes
 	if !bullseye.Bearing().IsMagnetic() {
 		log.Error().Stringer("bearing", bullseye.Bearing()).Msg("bearing provided to ComposeBullseye should be magnetic")
 	}
+	if bullseye.Distance().NauticalMiles() <= 5 {
+		return NaturalLanguageResponse{
+			Subtitle: "at bullseye",
+			Speech:   "at bullseye",
+		}
+	}
 	return NaturalLanguageResponse{
 		Subtitle: fmt.Sprintf(
 			"bullseye %s/%d",
