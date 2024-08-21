dab7aa3e590e4b7e314b260afd4d0da05968b38e
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2024-08-20 22:21:23 -0600

    Don't repeatedly broadcast clean PICTURE

--- pkg/controller/picture.go
+++ pkg/controller/picture.go
@@ -14,18 +14,24 @@ func (c *controller) HandlePicture(request *brevity.PictureRequest) {
 	logger := log.With().Str("callsign", request.Callsign).Type("type", request).Logger()
 	logger.Debug().Msg("handling request")
 
-	c.broadcastPicture(&logger)
+	c.broadcastPicture(&logger, true)
 }
 
-func (c *controller) broadcastPicture(logger *zerolog.Logger) {
+func (c *controller) broadcastPicture(logger *zerolog.Logger, forceBroadcast bool) {
 	count, groups := c.scope.GetPicture(conf.DefaultPictureRadius, c.coalition.Opposite(), brevity.FixedWing)
+	isPictureClean := count == 0
 	for _, group := range groups {
 		group.SetDeclaration(brevity.Hostile)
 	}
 
-	logger.Info().Int("groups", len(groups)).Int("count", count).Msg("broadcasting PICTURE")
-	c.out <- brevity.PictureResponse{Count: count, Groups: groups}
+	if c.wasLastPictureClean && isPictureClean && !forceBroadcast {
+		logger.Info().Msg("skipping PICTURE broadcast because situation has not changed since last broadcast")
+	} else {
+		logger.Info().Int("groups", len(groups)).Int("count", count).Msg("broadcasting PICTURE")
+		c.out <- brevity.PictureResponse{Count: count, Groups: groups}
+	}
 
 	c.pictureBroadcastDeadline = time.Now().Add(c.pictureBroadcastInterval)
-	logger.Info().Time("deadline", c.pictureBroadcastDeadline).Msg("extending next PICTURE broadcast time")
+	c.wasLastPictureClean = isPictureClean
+	logger.Info().Time("deadline", c.pictureBroadcastDeadline).Msg("extended next PICTURE broadcast time")
 }
