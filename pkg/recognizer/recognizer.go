d28c970ba72457bfc05f2d6f609770e93a49355a
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2024-08-21 21:48:57 -0600

    Limit speech recognition function to 30 seconds

--- pkg/recognizer/recognizer.go
+++ pkg/recognizer/recognizer.go
@@ -1,8 +1,10 @@
 // package recognizer recognizes text from speech
 package recognizer
 
+import "context"
+
 // Recognizer recognizes text from speech
 type Recognizer interface {
 	// Recognize takes PCMF32LE audio data and returns any recognized text.
-	Recognize([]float32) (string, error)
+	Recognize(context.Context, []float32) (string, error)
 }
