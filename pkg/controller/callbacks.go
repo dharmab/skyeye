4458aca9097431b81065258fcc6f4f3065ed6060
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2024-10-09 19:14:34 -0600

    Only broadcast FADED calls when contact is near a friendly aircraft

---
+++ pkg/controller/callbacks.go
@@ -0,0 +1,47 @@
+package controller
+
+import (
+	"github.com/dharmab/skyeye/pkg/brevity"
+	"github.com/dharmab/skyeye/pkg/coalitions"
+	"github.com/dharmab/skyeye/pkg/traces"
+	"github.com/dharmab/skyeye/pkg/trackfiles"
+	"github.com/martinlindhe/unit"
+	"github.com/paulmach/orb"
+	"github.com/rs/zerolog/log"
+)
+
+var fadeBroadcastRadius = 55 * unit.NauticalMile
+
+func (c *controller) handleFaded(location orb.Point, group brevity.Group, coalition coalitions.Coalition) {
+	for _, id := range group.ObjectIDs() {
+		c.remove(id)
+	}
+	isHostile := coalition == c.coalition.Opposite()
+	areHumansOnFrequency := c.srsClient.HumansOnFrequency() > 0
+	nearbyFriendlies := c.scope.FindNearbyGroupsWithBullseye(
+		location,
+		lowestAltitude,
+		highestAltitude,
+		fadeBroadcastRadius,
+		c.coalition,
+		brevity.Aircraft,
+		[]uint64{},
+	)
+	isNearFriendly := len(nearbyFriendlies) > 0
+
+	if isHostile && isNearFriendly && areHumansOnFrequency {
+		log.Info().Stringer("group", group).Msg("broadcasting FADED call")
+		group.SetDeclaration(brevity.Hostile)
+		c.calls <- NewCall(traces.NewRequestContext(), brevity.FadedCall{Group: group})
+	} else {
+		log.Debug().
+			Bool("isHostile", isHostile).
+			Bool("isNearFriendly", isNearFriendly).
+			Bool("areHumansOnFrequency", areHumansOnFrequency).
+			Msg("skipping FADED call because broadcast criteria are not met")
+	}
+}
+
+func (c *controller) handleRemoved(trackfile trackfiles.Trackfile) {
+	c.remove(trackfile.Contact.ID)
+}
--- pkg/radar/callbacks.go
+++ pkg/radar/callbacks.go
@@ -4,11 +4,12 @@ import (
 	"github.com/dharmab/skyeye/pkg/brevity"
 	"github.com/dharmab/skyeye/pkg/coalitions"
 	"github.com/dharmab/skyeye/pkg/trackfiles"
+	"github.com/paulmach/orb"
 )
 
 // FadedCallback is a callback function that is called when a group has not been updated by sensors for a timeout period.
 // The group and its coalition are provided.
-type FadedCallback func(group brevity.Group, coalition coalitions.Coalition)
+type FadedCallback func(location orb.Point, group brevity.Group, coalition coalitions.Coalition)
 
 func (s *scope) SetFadedCallback(callback FadedCallback) {
 	s.fadedCallback = callback
