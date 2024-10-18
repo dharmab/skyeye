f9d862124f87dfe633d69f2237e40620991a6154
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2024-10-18 13:26:06 -0600

    Simplify merged call

--- pkg/composer/merged.go
+++ pkg/composer/merged.go
@@ -1,19 +1,15 @@
 package composer
 
 import (
-	"fmt"
 	"strings"
 
 	"github.com/dharmab/skyeye/pkg/brevity"
 )
 
 func (c *composer) ComposeMergedCall(call brevity.MergedCall) NaturalLanguageResponse {
-	callsignList := strings.Join(call.Callsigns, ", ")
-	group := c.ComposeMergedWithGroup(call.Group)
-	template := "%s, merged, %s."
-
+	s := strings.Join(call.Callsigns, ", ") + ", merged."
 	return NaturalLanguageResponse{
-		Subtitle: fmt.Sprintf(template, callsignList, group.Subtitle),
-		Speech:   fmt.Sprintf(template, callsignList, group.Speech),
+		Subtitle: s,
+		Speech:   s,
 	}
 }
