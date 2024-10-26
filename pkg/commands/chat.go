3f0e8c2129085ab12ed618dd97f9345c9bd7663a
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2024-10-26 16:30:37 -0600

    Additional wiring and retries for chat requests

--- pkg/commands/chat.go
+++ pkg/commands/chat.go
@@ -2,6 +2,8 @@ package commands
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	"github.com/dharmab/skyeye/pkg/coalitions"
 	"github.com/lithammer/shortuuid/v3"
@@ -26,11 +28,13 @@ func NewChatListener(
 	callsign string,
 	missionClient mission.MissionServiceClient,
 	coalitionClient coalition.CoalitionServiceClient,
+	netClient net.NetServiceClient,
 ) *ChatListener {
 	manager := &ChatListener{
 		callsign:        callsign,
 		missionClient:   missionClient,
 		coalitionClient: coalitionClient,
+		netClient:       netClient,
 	}
 	if coalition == coalitions.Red {
 		manager.coalition = common.Coalition_COALITION_RED
@@ -40,71 +44,79 @@ func NewChatListener(
 	return manager
 }
 
-func (m *ChatListener) isPlayerOnCoalition(ctx context.Context, id uint32) (bool, error) {
-	request := &coalition.GetPlayerUnitsRequest{
-		Coalition: m.coalition,
-	}
-	units, err := m.coalitionClient.GetPlayerUnits(ctx, request)
-	if err != nil {
-		return false, err
-	}
-	for _, u := range units.GetUnits() {
-		if u.GetId() == id {
-			return true, nil
-		}
-	}
-	return false, nil
-}
-
 func (l *ChatListener) Run(ctx context.Context, messages chan<- Request) {
-	streamer, err := l.missionClient.StreamEvents(ctx, &mission.StreamEventsRequest{})
-	if err != nil {
-		log.Error().Err(err).Msg("error creating event stream")
-		return
-	}
+	nextAttempt := time.Now()
 	for {
 		select {
 		case <-ctx.Done():
 			log.Info().Msg("stopping chat listener due to context cancellation")
 			return
 		default:
-			response, err := streamer.Recv()
+			time.Sleep(time.Until(nextAttempt))
+			nextAttempt = time.Now().Add(5 * time.Second)
+			streamer, err := l.missionClient.StreamEvents(ctx, &mission.StreamEventsRequest{})
 			if err != nil {
-				log.Error().Err(err).Msg("event stream error")
+				log.Error().Err(err).Msg("error streaming chat events from DCS-gRPC")
 				continue
 			}
-			event := response.GetPlayerSendChat()
-			logger := log.With().Uint32("unitID", event.GetPlayerId()).Str("text", event.GetMessage()).Logger()
-			isSameCoalition, err := l.isPlayerOnCoalition(ctx, event.GetPlayerId())
+			if err = l.receive(ctx, streamer, messages); err != nil {
+				continue
+			}
+			log.Error().Err(err).Msg("error streaming chat messages")
+		}
+	}
+}
+
+func (l *ChatListener) receive(ctx context.Context, client mission.MissionService_StreamEventsClient, messages chan<- Request) error {
+	for {
+		select {
+		case <-ctx.Done():
+			log.Info().Msg("stopping chat listener due to context cancellation")
+			return nil
+		default:
+			response, err := client.Recv()
 			if err != nil {
-				logger.Error().Err(err).Msg("error checking player coalition")
+				return fmt.Errorf("event stream error: %w", err)
+			}
+			chatEvent := response.GetPlayerSendChat()
+			if chatEvent == nil {
 				continue
 			}
-			if !isSameCoalition {
-				logger.Debug().Msg("player is not on the same coalition")
+			if chatEvent.GetMessage() == "" {
 				continue
 			}
+			logger := log.With().Uint32("unitID", chatEvent.GetPlayerId()).Str("text", chatEvent.GetMessage()).Logger()
+			logger.Debug().Msg("received chat message")
 
 			players, err := l.netClient.GetPlayers(ctx, &net.GetPlayersRequest{})
 			if err != nil {
-				logger.Error().Err(err).Msg("error getting players")
+				return fmt.Errorf("error getting players: %w", err)
+			}
+
+			var playerInfo *net.GetPlayersResponse_GetPlayerInfo
+			for _, player := range players.GetPlayers() {
+				if player.GetId() == chatEvent.GetPlayerId() {
+					playerInfo = player
+				}
+			}
+			if playerInfo == nil {
+				logger.Debug().Msg("player not found")
 				continue
 			}
 
-			var playerName string
-			for _, player := range players.GetPlayers() {
-				if player.GetId() == event.GetPlayerId() {
-					playerName = player.GetName()
-					logger = logger.With().Str("player", playerName).Logger()
-				}
+			logger = logger.With().Str("name", playerInfo.GetName()).Stringer("coaltion", playerInfo.GetCoalition()).Logger()
+
+			if playerInfo.GetCoalition() != l.coalition {
+				logger.Debug().Msg("player not on coalition")
+				continue
 			}
 
 			logger.Info().Msg("received chat message")
 
 			messages <- Request{
 				TraceID:    shortuuid.New(),
-				PlayerName: playerName,
-				Text:       event.GetMessage(),
+				PlayerName: playerInfo.GetName(),
+				Text:       chatEvent.GetMessage(),
 			}
 		}
 	}
