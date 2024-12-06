1ea482aa8fdf4a48fe42865a1458bea8c5bf3190
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2024-12-05 22:14:38 -0700

    Refine DECLARE text

--- pkg/composer/declare.go
+++ pkg/composer/declare.go
@@ -10,7 +10,7 @@ import (
 // ComposeDeclareResponse constructs natural language brevity for responding to a DECLARE call.
 func (c *Composer) ComposeDeclareResponse(response brevity.DeclareResponse) NaturalLanguageResponse {
 	if response.Sour {
-		reply := fmt.Sprintf("%s, %s.", c.composeCallsigns(response.Callsign), "Unable, timber sour. Repeat your request with bullseye or BRAA position included.")
+		reply := fmt.Sprintf("%s, %s", c.composeCallsigns(response.Callsign), "unable, timber sour. Repeat your request with bullseye or BRAA position included.")
 		return NaturalLanguageResponse{
 			Subtitle: reply,
 			Speech:   reply,
