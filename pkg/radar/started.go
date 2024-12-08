6a2de4f35791e49c0670fe2437728333a90b88f1
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2024-12-07 20:18:44 -0700

    Deduplicate FADED calls

--- pkg/radar/started.go
+++ pkg/radar/started.go
@@ -1,10 +1,26 @@
 package radar
 
-import "github.com/rs/zerolog/log"
+import (
+	"time"
+
+	"github.com/dharmab/skyeye/pkg/sim"
+	"github.com/rs/zerolog/log"
+)
 
 func (r *Radar) handleStarted() {
 	log.Info().Msg("clearing all trackfiles due to mission (re)start")
 	r.contacts.reset()
+
+	log.Info().Msg("clearing pending FADED trackfiles due to mission (re)start")
+	r.pendingFadesLock.Lock()
+	defer r.pendingFadesLock.Unlock()
+	r.pendingFades = make([]sim.Faded, 0)
+
+	log.Info().Msg("clearing FADED trackfile history due to mission (re)start")
+	r.completedFadesLock.Lock()
+	defer r.completedFadesLock.Unlock()
+	r.completedFades = make(map[uint64]time.Time)
+
 	r.callbackLock.RLock()
 	defer r.callbackLock.RUnlock()
 	if r.startedCallback != nil {
