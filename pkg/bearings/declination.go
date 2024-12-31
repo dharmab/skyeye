26003318c5772c21674ed6ac1475413c91d09efe
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2024-12-30 18:39:47 -0700

    Fix Y2K25 bug in magnetic declination function

--- pkg/bearings/declination.go
+++ pkg/bearings/declination.go
@@ -11,12 +11,12 @@ import (
 )
 
 var igrfData = igrf.New()
-var stubDate = time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC)
+var stubDate = time.Date(2023, 1, 1, 0, 0, 0, 0, time.UTC)
 
 // Declination returns the magnetic declination at the given point and time.
 func Declination(p orb.Point, t time.Time) (unit.Angle, error) {
-	if 1900 > t.Year() || t.Year() > 2025 {
-		log.Warn().Time("date", t).Msg("year is outside IGRF model range, replacing with 2024")
+	if 1900 > t.Year() || t.Year() > stubDate.Year() {
+		log.Warn().Time("date", t).Msgf("year is outside IGRF model range, replacing with %d", stubDate.Year())
 		t = stubDate
 	}
 	field, err := igrfData.IGRF(p.Lat(), p.Lon(), 0, float64(t.Year())+float64(t.YearDay())/366)
