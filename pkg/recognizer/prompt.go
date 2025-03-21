2d4b6342eb8c1d91ffa3ab50d604f8ac7a17e716
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2025-03-20 21:19:25 -0600

    Add support for GPT-4o transcription models

--- pkg/recognizer/prompt.go
+++ pkg/recognizer/prompt.go
@@ -2,7 +2,7 @@ package recognizer
 
 import "fmt"
 
-// prompt constructs a prompt for OpenAI's Whisper model. See https://platform.openai.com/docs/guides/speech-to-text#prompting
+// prompt constructs a prompt for OpenAI's audio transcription models. See https://platform.openai.com/docs/guides/speech-to-text#prompting
 func prompt(callsign string) string {
 	return fmt.Sprintf("Either ANYFACE or %s, PILOT CALLSIGN, DIGITS, one of 'RADIO' 'ALPHA' 'BOGEY' 'PICTURE' 'DECLARE' 'SNAPLOCK' 'SPIKED', ARGUMENTS such as BULLSEYE, BRAA, numbers or digits.", callsign)
 }
