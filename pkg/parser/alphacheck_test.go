a15438b0b7471968ee96540399e235e8a7eb3709
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2025-04-21 19:14:59 -0600

    WIP: Custom locations and VECTOR command

--- pkg/parser/alphacheck_test.go
+++ pkg/parser/alphacheck_test.go
@@ -65,7 +65,7 @@ func TestParserAlphaCheck(t *testing.T) {
 			},
 		},
 	}
-	runParserTestCases(t, New(TestCallsign, true), testCases, func(t *testing.T, test parserTestCase, request any) {
+	runParserTestCases(t, New(TestCallsign, []string{}, true), testCases, func(t *testing.T, test parserTestCase, request any) {
 		t.Helper()
 		expected := test.expected.(*brevity.AlphaCheckRequest)
 		actual := request.(*brevity.AlphaCheckRequest)
