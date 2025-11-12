cf1ce2f2245145083d88f03169b2798c3b9f6d62
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2025-11-11 19:07:03 -0700

    Time out synthesis after 30 seconds

--- internal/application/synthesize.go
+++ internal/application/synthesize.go
@@ -27,8 +27,8 @@ func (a *Application) synthesize(ctx context.Context, in <-chan Message[composer
 
 // synthesizeMessage synthesizes a single message and publishes the audio to the output channel.
 func (a *Application) synthesizeMessage(ctx context.Context, response composer.NaturalLanguageResponse, out chan<- Message[simpleradio.Audio]) {
-	lockCtx, cancel := context.WithTimeout(ctx, 30*time.Second)
-	defer cancel()
+	lockCtx, lockCancel := context.WithTimeout(ctx, 30*time.Second)
+	defer lockCancel()
 	if err := tryLock(lockCtx, a.speakerLock); err != nil {
 		err := fmt.Errorf("unable to obtain speaker lock: %w", err)
 		log.Error().Err(err).Msg("error synthesizing speech")
@@ -39,7 +39,9 @@ func (a *Application) synthesizeMessage(ctx context.Context, response composer.N
 
 	log.Info().Str("text", response.Speech).Msg("synthesizing speech")
 	start := time.Now()
-	audio, err := a.speaker.SayContext(ctx, response.Speech)
+	synthesisCtx, synthesisCancel := context.WithTimeout(ctx, 30*time.Second)
+	defer synthesisCancel()
+	audio, err := a.speaker.SayContext(synthesisCtx, response.Speech)
 	if err != nil {
 		log.Error().Err(err).Msg("error synthesizing speech")
 		a.trace(traces.WithRequestError(ctx, err))
